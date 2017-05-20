480201a902483533024f3255560cd98ac7f8a51d
Michael Jennings <zeth.jennings@gmail.com>
2017-05-20 05:46:33 -0700

    setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,13 @@
+
+import 'hammerjs';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MdButtonModule, MdCheckboxModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +18,12 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+
+    FlexLayoutModule,
+    MdButtonModule,
+    MdCheckboxModule,
+
   ],
   providers: [],
   bootstrap: [AppComponent]
